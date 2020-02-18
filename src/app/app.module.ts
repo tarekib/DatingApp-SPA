56ee9d4962cf717863c22fc7f9b017860225c65b
Tarek Ibrahim <tarek.ibrahim@ecomz.com>
2020-02-18 14:15:44 -0800

    client side register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
